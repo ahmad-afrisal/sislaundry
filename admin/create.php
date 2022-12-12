41afab7d4cf2543e736b54ada43a2c096fb32d58
wulandarikaranamu02@gmail.com <wulandarikaranamu02@gmail.com>
2022-12-12 00:49:21 -0800

    update login

--- admin/create.php
+++ admin/create.php
@@ -259,7 +259,7 @@ if (isset($_POST["register"])) {
                           <label class="form-label" for="confpassword">Konfirmasi Password</label>
                           <input type="password" class="form-control" id="confpassword" name="confpassword" placeholder="" required/>
                         </div>
-                        <button type="submit" class="btn btn-primary">Simpan</button>
+                        <button type="submit" name="register" class="btn btn-primary">Simpan</button>
                         <a href="index.html" class="btn btn-secondary">Kembali</a>
                       </form>
 
